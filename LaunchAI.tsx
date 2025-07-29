0b48916cbd8e8da76804bcc8ebc16b1bcc220a7f
ProMarketnet <scandi1803@gmail.com>
2025-07-29 14:33:45 -0500

    Update LaunchAI.tsx

--- LaunchAI.tsx
+++ LaunchAI.tsx
@@ -299,7 +299,7 @@ export default function LaunchAI() {
   }
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white"></div>
       {/* Header */}
       <header className="border-b border-gray-100">
         <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
