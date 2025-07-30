1b5e3b4326e1fad0bcd464499df83420b19f58ef
ProMarketnet <scandi1803@gmail.com>
2025-07-30 10:26:40 -0500

    Create Layout.tsx

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+// components/Layout.tsx
+import { ReactNode } from 'react';
+import { Navigation } from './Navigation';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navigation />
+      <main className="flex-1">
+        {children}
+      </main>
+    </div>
+  );
+};
